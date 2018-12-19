e0836e69c2903f47df8aeac3c35502f8862cd339
zhensun <zhen.sun@westwell-lab.com>
2018-12-19 17:55:20 +0800

    densenet

---
+++ cifar_test.py
@@ -0,0 +1,64 @@
+import os
+
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+
+from model import *
+
+
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(testset, batch_size=50, shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+model = DenseNet121()
+model = model.cuda()
+
+optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
+
+
+for epoch in range(100):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs
+        inputs, labels = data
+        inputs = inputs.cuda()
+        labels = labels.cuda()
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+    if epoch % 10 == 0:    # print every 2000 mini-batches
+        print('[%d] loss: %.3f' %
+                (epoch + 1, running_loss / trainloader.__len__()))
+
+print('Finished Training')
